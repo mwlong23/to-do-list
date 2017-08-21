41677ab424483418f49c7fc83db2b47498c7d54d
Kristen Kulha and Mitchell Long <mitchell@>
2017-08-21 14:45:57 -0700

    Remove priority from text output.

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
     var newTask = new Task(toDo, priority);
     var context = priorityContext(priority);
     console.log(priority);
-    $("#list").append('<li class=' + context + '>' + newTask.priority + " " + newTask.toDo + '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span></li>');
+    $("#list").append('<li class=' + context + '>' + " " + newTask.toDo + '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span></li>');
     $("ul#list").children("li").last().click(function(){
       $(this).remove();
     });
