75e849fe3c37d7bab34b443ba28562bcb225cb4a
Kristen Kulha and Mitchell Long <kristen@>
2017-08-21 13:22:20 -0700

    append to do item to right column

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+//Business Logic
+function Task(toDo, priority) {
+  this.toDo = toDo;
+  this.priority = priority;
+}
 
+
+
+// UI logic
+$(document).ready(function(){
+  $("form#to-do").submit(function(event){
+    event.preventDefault();
+    var toDo = $("#task").val();
+    var priority = $("#priority").val();
+    var newTask = new Task(toDo, priority);
+    console.log(priority, newTask);
+    console.log(toDo);
+    $("#list").append("<li>" + newTask.toDo + " " + newTask.priority + "</li>");
+  });
+});
